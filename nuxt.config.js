1f6321b02823107f91ea131bf9da68a42463b1ad
Jeny <853297695@qq.com>
2020-01-16 14:43:37 +0800

    修改配置文件 下载less less-loader包

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,17 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    // todo 第一个修改的地方
+    title: '世界上最特别的旅游网站',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
+    // todo 第二个修改的地方 新增图标样式
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css' }
     ]
   },
   /*
@@ -22,8 +25,10 @@ export default {
   /*
   ** Global CSS
   */
+  // todo 第三个修改的地方 
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    'assets/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
@@ -44,12 +49,18 @@ export default {
   /*
   ** Build configuration
   */
+ // todo 第四个修改的地方 
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    // baseURL: "http://157.122.54.189:9095" // 新增备用地址
+    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径 		  
+  },
   build: {
     transpile: [/^element-ui/],
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
